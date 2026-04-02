584a15e8e9485508792603ab85f5cd925aa9f1c9
Ejay G. Rulona <ejayrulona@gmail.com>
2026-04-02 22:09:12 +0800

    Registered users app models in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import CollegeOffice, AdminProfile, CollegeProfile 
 
 # Register your models here.
+
+admin.site.register(CollegeOffice)
+admin.site.register(AdminProfile)
+admin.site.register(CollegeProfile)
