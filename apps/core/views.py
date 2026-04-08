35439dd3960ff98b4cc0ff2c3562c4462ccda418
ViktorGreg <limviktorgreg14@gmail.com>
2026-04-08 21:48:46 +0800

    College/Office side
    Updates:
    * base.html
    
    College/Office Side:
    * Dashboard
    * Drafts
    * ppmp_form(wag muna to hawakan)
    * ppmp
    * lists
    * request_detail(not finished yet)
    
    Note: if want mo makita yung proper viewing, lagay mo temporary sa core/templates/core/ yung html file and uncomment mo lang yung mga naka comment sa views.py and urls.py.

--- apps/core/views.py
+++ apps/core/views.py
@@ -5,3 +5,32 @@ def home(request):
 
 def dashboard(request):
     return render(request, "core/dashboard.html")
+
+# Pwede mo na to remove mga naka comment, nilagay ko lang here sa core para ma view ko siya sa Web maayos, wag kalimutan remove din pati sa urls.py 
+
+# def draft(request):
+#     return render(request, "core/draft.html")
+
+# def list_page(request):
+#     return render(request, "core/list_page.html")
+
+# def detailed_view(request):
+#     return render(request, "core/detailed_view.html")
+
+# def college_dashboard(request):
+#     return render(request, "core/college_dashboard.html")
+
+# def ppmp_form(request):
+#     return render(request, "core/ppmp_form.html")
+
+# def request(request):
+#     return render(request, "core/request.html")
+
+# def ppmp(request):
+#     return render(request, "core/ppmp.html")
+
+# def drafts(request):
+#     return render(request, "core/drafts.html")
+
+# def request_detail(request):
+#     return render(request, "core/request_detail.html")
