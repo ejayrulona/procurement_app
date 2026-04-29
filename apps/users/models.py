e114e08cf32fabfe991644ef3efe76ae7a3f458c
Ejay G. Rulona <ejayrulona@gmail.com>
2026-04-29 18:34:22 +0800

    Added updated_at field for User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -77,6 +77,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
