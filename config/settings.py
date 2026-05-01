1dc1a3f81bb9858dec8aa8b49c239bfb1efc49a2
Ejay G. Rulona <ejayrulona@gmail.com>
2026-05-01 19:56:06 +0800

    Created decorators in users app for role based view access, assigned role access for each working pages, updated custom login and renamed dashboard.html to admin_dashboard.html

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
-LOGIN_REDIRECT_URL = "core:dashboard"
 LOGIN_URL = "core:home"
 LOGOUT_REDIRECT_URL = "core:home"
 
