5b4cc1a0da4391c21cb69432af39d8f5a8b693b1
ViktorGreg <limviktorgreg14@gmail.com>
2026-05-03 10:38:27 +0800

    Modified Inventory, Added Full viewing and edit of Inventory.

--- apps/inventory/urls.py
+++ apps/inventory/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = "inventory"
+app_name = 'inventory'
 
-urlpatterns = (
-    path("inventory/", views.list_inventory, name="list_inventory"),
-)
+urlpatterns = [
+    path('inventory/', views.list_inventory, name='list_inventory'),
+    path('view/<str:item_id>/', views.view_inventory_item, name='view_inventory_item'),
+    path('edit/<str:item_id>/', views.edit_inventory_item, name='edit_inventory_item'),
+]
--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def list_inventory(request):
     return render(request, "inventory/inventory.html")
+
+def view_inventory_item(request, item_id):
+    context = {
+        'item_id': item_id
+    }
+    return render(request, "inventory/inventory_detail_view.html", context)
+
+def edit_inventory_item(request, item_id):
+    context = {
+        'item_id': item_id
+    }
+    return render(request, "inventory/inventory_edit.html", context)
