459b70ed720880aaf5a7fba844921c80c5e3bcd0
Ejay G. Rulona <ejayrulona@gmail.com>
2026-05-20 12:07:51 +0800

    Updated activity_log veiw function to show only the activities of their own for the office users

--- apps/activity_logs/views.py
+++ apps/activity_logs/views.py
@@ -1,21 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import ActivityLog
 
+@login_required
 def activity_log(request):
-    activities = ActivityLog.objects.prefetch_related("user").order_by("-timestamp")
+    if request.user.is_any_admin:
+        activities = ActivityLog.objects.prefetch_related("user").order_by("-timestamp")
+        user_activities = activities.filter(target_type=ActivityLog.TargetType.USER).count()
+        item_activities = activities.filter(target_type=ActivityLog.TargetType.ITEM).count()
+        app_activities = activities.filter(target_type=ActivityLog.TargetType.APP).count()
+    else:
+        activities = ActivityLog.objects.filter(user=request.user).prefetch_related("user").order_by("-timestamp")
 
     total_activities = activities.count()
-    user_activities = activities.filter(target_type=ActivityLog.TargetType.USER).count()
-    item_activities = activities.filter(target_type=ActivityLog.TargetType.ITEM).count()
     ppmp_activities = activities.filter(target_type=ActivityLog.TargetType.PPMP).count()
-    app_activities = activities.filter(target_type=ActivityLog.TargetType.APP).count()
 
     context = {
         "activities": activities,
         "total_activities": total_activities,
-        "user_activities": user_activities,
-        "item_activities": item_activities,
         "ppmp_activities": ppmp_activities,
-        "app_activities": app_activities,
     }
+
+    if request.user.is_any_admin:
+        context.update({
+            "user_activities": user_activities,
+            "item_activities": item_activities,
+            "app_activities": app_activities,
+        })
+
     return render(request, "activity_logs/activity_log.html", context)
